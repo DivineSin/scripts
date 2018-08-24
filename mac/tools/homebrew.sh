09e436e1d0a33ba58ba688514c266138f8c16b82
Tianyu Bai <t3bai@eng.ucsd.edu>
2018-08-24 15:58:54 +0800

    🐛

--- mac/tools/homebrew.sh
+++ mac/tools/homebrew.sh
@@ -2,7 +2,7 @@
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bash_profile  
+echo "export PATH=/usr/local/bin:$PATH" >> ~/.bash_profile  
 
 source ~/.bash_profile
 
