da855c636ba974ea6e67b5b7d09c18a24becafec
Tianyu Bai <t3bai@eng.ucsd.edu>
2018-10-03 16:21:45 -0700

    :rocket:

--- ubuntu/tools/dlib.sh
+++ ubuntu/tools/dlib.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+cd ~
 git clone https://github.com/davisking/dlib.git
-
+cd dlib
 mkdir build
 cd build
-cmake .. -DUSE_AVX_INSTRUCTIONS=1; cmake --build .
-python setup.py install
+cmake ..
+cmake --build . --config Release
+sudo make install
+sudo ldconfig
+cd ..
+sudo python setup.py install
+sudo rm -rf dist
+sudo rm -rf tools/python/build
--- ubuntu/tools/protobuf.sh
+++ ubuntu/tools/protobuf.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-
+cd /tmp
+sudo apt-get install autoconf automake libtool curl make g++ unzip
 git clone https://github.com/protocolbuffers/protobuf.git
 cd protobuf
 git submodule update --init --recursive
